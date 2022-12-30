09789d99803c676fcc77eba46440d53c20ac7e1f
BOJAN SAPUNOV <119940288+BojanSapunov@users.noreply.github.com>
2022-12-30 10:11:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-i = 0
 
-while i<5:
-  streamlit.text(i)
-  i+=1
-  
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boild Free-Range Egg')
